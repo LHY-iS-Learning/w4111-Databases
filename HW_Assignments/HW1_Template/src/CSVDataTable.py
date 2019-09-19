bee3bb45e89147e4d66fade33c247f2cf7ab7846
LHY-iS-Learning <hongyi.lyu95@gmail.com>
2019-09-19 16:52:34 -0400

    add corner cases

--- HW_Assignments/HW1_Template/src/CSVDataTable.py
+++ HW_Assignments/HW1_Template/src/CSVDataTable.py
@@ -33,6 +33,8 @@ class CSVDataTable(BaseDataTable):
             "debug": debug
         }
 
+        self._primary_keys_set = set()
+
         self._logger = logging.getLogger()
 
         self._logger.debug("CSVDataTable.__init__: data = " + json.dumps(self._data, indent=2))
@@ -69,9 +71,18 @@ class CSVDataTable(BaseDataTable):
         return result
 
     def _add_row(self, r):
+        key_col = list(self._data["key_columns"])
         if self._rows is None:
             self._rows = []
-        self._rows.append(r)
+        this_key_col_val = ""
+        for key in key_col:
+            if key not in r.keys() or not len(r[key]):
+                raise Exception("Some row does not have primary key info!")
+            this_key_col_val += (r[key] + '_')
+        this_key_col_val = this_key_col_val[:-1]
+        if this_key_col_val not in self._primary_keys_set:
+            self._rows.append(r)
+            self._primary_keys_set.add(this_key_col_val)
 
     def _load(self):
 
@@ -79,9 +90,17 @@ class CSVDataTable(BaseDataTable):
         file_n = self._data["connect_info"].get("file_name")
         full_name = os.path.join(dir_info, file_n)
 
+        table_col = None
+
         with open(full_name, "r") as txt_file:
             csv_d_rdr = csv.DictReader(txt_file)
             for r in csv_d_rdr:
+                if table_col is None:
+                    table_col = list(r.keys())
+                    key_col = self._data.get("key_columns", None)
+                    if key_col is not None and not set(key_col).issubset(table_col):
+                        raise Exception("Key Column is not the subset of All Columns!")
+                    self._data["table_columns"] = table_col
                 self._add_row(r)
 
         self._logger.debug("CSVDataTable._load: Loaded " + str(len(self._rows)) + " rows")
@@ -114,7 +133,8 @@ class CSVDataTable(BaseDataTable):
 
         return result
 
-    def _copy_select_fields(self, result_dic, field_list = None):
+    @staticmethod
+    def _copy_select_fields(result_dic, field_list=None):
         if field_list is None:
             return result_dic
         else:
@@ -122,6 +142,23 @@ class CSVDataTable(BaseDataTable):
             for field in field_list:
                 res[field] = result_dic[field]
             return res
+
+    def validate_template_and_fields(self, template=None, fields=None):
+        col_set = set(self._data["table_columns"])
+
+        t_set = None
+        f_set = None
+        if template is not None:
+            t_set = set(template)
+        if fields is not None:
+            f_set = set(fields)
+
+        if t_set is not None and not t_set.issubset(col_set):
+            raise Exception("template column is not a subset of table columns!")
+        if f_set is not None and not f_set.issubset(col_set):
+            raise Exception("field column is not a subset of table columns!")
+
+        return True
 
     def find_by_primary_key(self, key_fields, field_list=None):
         """
@@ -133,7 +170,8 @@ class CSVDataTable(BaseDataTable):
         """
 
         # TODO: handle no primary key
-        # TODO: error field_list
+        self.validate_template_and_fields(fields=field_list)
+
         primary_key_lst = self._data.get("key_columns")
         result_dic = None
         for row in self._rows:
@@ -167,13 +205,8 @@ class CSVDataTable(BaseDataTable):
         result_list = []
 
         for row in self._rows:
-            all_match = True
-            for k,v in template.items():
-                if row[k] != v:
-                    all_match = False
-                    break
-            if all_match:
-                result_list.append(dict(row))
+            if self.matches_template(row, template):
+                result_list.append(row)
 
         if field_list is None:
             return result_list
